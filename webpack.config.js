f67ec8e93d8deba35d8e3b0e3c7c0f14815c869f
Sean MacDonald <147445255+sfmacdonald@users.noreply.github.com>
2024-04-08 11:54:24 -0700

    Add css-loader and style-loader for CSS support

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Add CSS loader support
+      },
     ],
   },
   resolve: {
