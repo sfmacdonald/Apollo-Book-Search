895d598bdbb978e6be19f6ead8a163f826f912b9
Sean MacDonald <147445255+sfmacdonald@users.noreply.github.com>
2024-04-23 16:13:06 -0700

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Define the mongoURI variable with an environment variable or default to localhost
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
+// Use the mongoURI in your connection setup
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
